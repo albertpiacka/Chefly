aa3e74659ecdc4b7e549d28979108004b4f1c7df
albertpiacka <albertpiacka@gmail.com>
2020-11-02 20:06:36 +0100

    subpage for user we are searching for

---
+++ resources/views/users/user.blade.php
@@ -0,0 +1,9 @@
+<div class="container py-5">
+    <div class="flex-center position-ref full-height">
+        <div class="content">
+
+            <div class="posts" data-aos="fade-up">
+                <h1>{{$user->name}}</h1>
+            </div>
+        </div>
+    </div>
