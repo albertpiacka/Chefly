98233670f6f87f21a699e101d18c86ac2ab6cf04
albertpiacka <albertpiacka@gmail.com>
2020-11-04 11:17:10 +0100

    main page posts section is simplified

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,34 +10,23 @@
             </div>
             
             <div class="posts" data-aos="fade-up">
-                {{-- @foreach ($posts as $post)
-                    <div class="post">
-                        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrows-fullscreen" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd" d="M5.828 10.172a.5.5 0 0 0-.707 0l-4.096 4.096V11.5a.5.5 0 0 0-1 0v3.975a.5.5 0 0 0 .5.5H4.5a.5.5 0 0 0 0-1H1.732l4.096-4.096a.5.5 0 0 0 0-.707zm4.344 0a.5.5 0 0 1 .707 0l4.096 4.096V11.5a.5.5 0 1 1 1 0v3.975a.5.5 0 0 1-.5.5H11.5a.5.5 0 0 1 0-1h2.768l-4.096-4.096a.5.5 0 0 1 0-.707zm0-4.344a.5.5 0 0 0 .707 0l4.096-4.096V4.5a.5.5 0 1 0 1 0V.525a.5.5 0 0 0-.5-.5H11.5a.5.5 0 0 0 0 1h2.768l-4.096 4.096a.5.5 0 0 0 0 .707zm-4.344 0a.5.5 0 0 1-.707 0L1.025 1.732V4.5a.5.5 0 0 1-1 0V.525a.5.5 0 0 1 .5-.5H4.5a.5.5 0 0 1 0 1H1.732l4.096 4.096a.5.5 0 0 1 0 .707z"/>
-                        </svg>
-                        <h2 class="headings-primary-light text-left">{{$post->title}}</h2>
-                        <p class="text-left">{{$post->text}}</p>
-                        <small>{{$post->user->name}}</small>
-                    </div>
-                @endforeach --}}
                 <div class="post">
                     <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrows-fullscreen" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd" d="M5.828 10.172a.5.5 0 0 0-.707 0l-4.096 4.096V11.5a.5.5 0 0 0-1 0v3.975a.5.5 0 0 0 .5.5H4.5a.5.5 0 0 0 0-1H1.732l4.096-4.096a.5.5 0 0 0 0-.707zm4.344 0a.5.5 0 0 1 .707 0l4.096 4.096V11.5a.5.5 0 1 1 1 0v3.975a.5.5 0 0 1-.5.5H11.5a.5.5 0 0 1 0-1h2.768l-4.096-4.096a.5.5 0 0 1 0-.707zm0-4.344a.5.5 0 0 0 .707 0l4.096-4.096V4.5a.5.5 0 1 0 1 0V.525a.5.5 0 0 0-.5-.5H11.5a.5.5 0 0 0 0 1h2.768l-4.096 4.096a.5.5 0 0 0 0 .707zm-4.344 0a.5.5 0 0 1-.707 0L1.025 1.732V4.5a.5.5 0 0 1-1 0V.525a.5.5 0 0 1 .5-.5H4.5a.5.5 0 0 1 0 1H1.732l4.096 4.096a.5.5 0 0 1 0 .707z"/>
                     </svg>
-                    <h2 class="headings-primary-light text-left"><a href="/posts/{{$posts[0]->id}}">{{$posts[1]->title}}</a></h2>
-                    <p class="text-left">{{$posts[0]->text}}</p>
-                    <small>{{$posts[0]->user->name}}</small>
+                    <h2 class="headings-primary-light text-left"><a href="/posts/{{$posts[0]->slug}}">{{$posts[0]->title}}</a></h2>
+                    <p style="padding: 1em; text-indent: 4em" class="text-justify">{{$posts[0]->text}}</p>
+                    <h4><a href="/users/{{$posts[0]->user->id}}">{{$posts[0]->user->name}}</a></h4>
                 </div>
 
                 <div class="post">
                     <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrows-fullscreen" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd" d="M5.828 10.172a.5.5 0 0 0-.707 0l-4.096 4.096V11.5a.5.5 0 0 0-1 0v3.975a.5.5 0 0 0 .5.5H4.5a.5.5 0 0 0 0-1H1.732l4.096-4.096a.5.5 0 0 0 0-.707zm4.344 0a.5.5 0 0 1 .707 0l4.096 4.096V11.5a.5.5 0 1 1 1 0v3.975a.5.5 0 0 1-.5.5H11.5a.5.5 0 0 1 0-1h2.768l-4.096-4.096a.5.5 0 0 1 0-.707zm0-4.344a.5.5 0 0 0 .707 0l4.096-4.096V4.5a.5.5 0 1 0 1 0V.525a.5.5 0 0 0-.5-.5H11.5a.5.5 0 0 0 0 1h2.768l-4.096 4.096a.5.5 0 0 0 0 .707zm-4.344 0a.5.5 0 0 1-.707 0L1.025 1.732V4.5a.5.5 0 0 1-1 0V.525a.5.5 0 0 1 .5-.5H4.5a.5.5 0 0 1 0 1H1.732l4.096 4.096a.5.5 0 0 1 0 .707z"/>
                     </svg>
-                    <h2 class="headings-primary-light text-left"><a href="/posts/{{$posts[1]->id}}">{{$posts[1]->title}}</a></h2>
-                    <p class="text-left">{{$posts[1]->text}}</p>
-                    <small>{{$posts[1]->user->name}}</small>
+                    <h2 class="headings-primary-light text-left"><a href="/posts/{{$posts[1]->slug}}">{{$posts[1]->title}}</a></h2>
+                    <p style="padding: 1em; text-indent: 4em" class="text-justify">{{$posts[1]->text}}</p>
+                    <h4><a href="/users/{{$posts[1]->user->id}}">{{$posts[1]->user->name}}</a></h4>
                 </div>
-               
             </div>
 
             <div class="about-wrapper">
