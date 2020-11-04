65819bf7ece5f2fa97330492c20f1a814cc7c1de
albertpiacka <albertpiacka@gmail.com>
2020-11-04 11:22:09 +0100

    Changed name of new vue component

--- resources/js/app.js
+++ resources/js/app.js
@@ -19,7 +19,7 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('flash-message', require('./components/flashMessage.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -30,6 +30,11 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 const app = new Vue({
     el: '#app',
 });
+
+/**
+ * Inspired by DesignCourse >> https://www.youtube.com/watch?v=ZKXv_ZHQ654&t=927s&ab_channel=DesignCourse
+ * Dark / Light toggle mode
+ */
 
 var changeTheme = function () {
     var checkbox = document.querySelector('input[name=theme]'),
@@ -68,3 +73,45 @@ var changeTheme = function () {
 }();
 
 changeTheme.run();
+
+/**
+ * Inspired by Dev Ed >> https://www.youtube.com/watch?v=XK7T3mY1V-w&ab_channel=DevEd
+ * Neat 3D animation when you hover over article
+ */
+
+//Movement Animation to happen
+const card = document.querySelector(".post");
+const container = document.querySelector(".animation-container");
+
+const svg = document.querySelector(".post svg");
+const h2 = document.querySelector(".post h2");
+const h3 = document.querySelector(".post h3");
+const p = document.querySelector(".post p");
+
+const cardElements = [svg, h2, h3, p];
+
+if(container){
+    //Moving Animation Event
+    container.addEventListener("mousemove", (e) => {
+        let xAxis = (window.innerWidth / 2 - e.pageX) / 150;
+        let yAxis = (window.innerHeight - e.pageY) / 150;
+        card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+    });
+    //Animate In
+    container.addEventListener("mouseenter", (e) => {
+        card.style.transition = "none";
+    
+        cardElements.forEach(el => {
+            el.style.transform = "translateZ(50px)";
+        });
+    });
+    //Animate Out
+    container.addEventListener("mouseleave", (e) => {
+        card.style.transition = "all 0.5s ease";
+        card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+
+        cardElements.forEach(el => {
+            el.style.transform = "translateZ(0px)";
+        });
+    });
+}
