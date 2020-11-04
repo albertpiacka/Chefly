b27565e0993048a7c5ccdf28af6b04811e5d559e
albertpiacka <albertpiacka@gmail.com>
2020-11-04 11:25:15 +0100

    New comment controller, auth protection included

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Comment;
+use Illuminate\Http\Request;
+
+class CommentController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'text' => 'required',
+            'post_id' => 'required|integer|exists:posts,id'
+        ]);
+
+        $comment = auth()->user()->comments()->create(
+            $request->all()
+        );
+
+        return redirect('/posts/' . $comment->post->slug)->with('flash', 'Comment sucessfully added');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Comment  $comment
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Comment $comment)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Comment  $comment
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Comment $comment)
+    {
+        //
+    }
+}
