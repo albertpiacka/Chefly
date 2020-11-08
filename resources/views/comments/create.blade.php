7492c8b0c1c5876923cfdd8ee05d911cb0789064
albertpiacka <albertpiacka@gmail.com>
2020-11-08 21:11:42 +0100

    Adding comments via vue (axios)

--- resources/views/comments/create.blade.php
+++ resources/views/comments/create.blade.php
@@ -1,9 +1,13 @@
-<div class="add-comment form-group">
-    @include('errors.error')
-    <form action="/comments" method="post">
-        @csrf
-        <textarea name="text" rows="4" class="form-control" placeholder="Add comment"></textarea>
-        <input type="hidden" name="post_id" value="{{$post->id}}">
-        <input type="submit" class="button-primary-light">
-    </form>
-</div>
+<create-comment inline-template :post-data="{{$post}}">
+    <div class="add-comment form-group">
+        @include('errors.error')
+        <form action="#" method="post" @submit.prevent="returnData">
+            @csrf
+            <textarea ref="textArea" v-model="text" name="text" rows="4" class="form-control" placeholder="Add comment"></textarea>
+            <input ref="postId" type="hidden" name="post_id" value="{{$post->id}}">
+            <input ref="userId" type="hidden" name="user_id" value="{{$post->user->id}}">
+            <input ref="userName" type="hidden" name="user_name" value="{{Auth::user()->name}}">
+            <input type="submit" class="button-primary-light">
+        </form>
+    </div>
+</create-comment>
