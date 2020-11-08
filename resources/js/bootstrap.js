9acea5cf5f460939fe495fd94af0673f4f214f7a
albertpiacka <albertpiacka@gmail.com>
2020-11-08 21:31:16 +0100

    just added axios,momentjs,lodash

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,10 +1,7 @@
 window._ = require('lodash');
-
-/**
- * We'll load jQuery and the Bootstrap jQuery plugin which provides support
- * for JavaScript based Bootstrap features such as modals and tabs. This
- * code may be modified to fit the specific needs of your application.
- */
+window.Vue = require('vue');
+window.axios = require('axios');
+window.momentJs = require('moment');
 
 try {
     window.Popper = require('popper.js').default;
@@ -18,8 +15,6 @@ try {
  * to our Laravel back-end. This library automatically handles sending the
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
-
-window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
