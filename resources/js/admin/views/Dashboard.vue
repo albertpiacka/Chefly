31ba48c8f091b132e7f6c901ef47b8eccf4c3590
albertpiacka <albertpiacka@gmail.com>
2020-11-08 21:26:05 +0100

    Admin dashboard component

---
+++ resources/js/admin/views/Dashboard.vue
@@ -0,0 +1,15 @@
+<template>
+    <div data-aos="fade-up">
+        <h1>Dashboard</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
