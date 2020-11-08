4cab0d9ef8a0e1fe639c198a987334ee3e4452e8
albertpiacka <albertpiacka@gmail.com>
2020-11-08 21:06:30 +0100

    New api routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResources([
+    'users' => 'API\UserController',
+    'posts' => 'API\PostController',
+    'comments' => 'API\CommentController',
+]);
