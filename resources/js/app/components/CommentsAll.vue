1bd7055227bf1113ea9dde599d00367084dcce95
albertpiacka <albertpiacka@gmail.com>
2020-11-08 21:23:09 +0100

    We can render all comments for specific post

---
+++ resources/js/app/components/CommentsAll.vue
@@ -0,0 +1,51 @@
+
+
+<script>
+    export default {
+        props: ['post-data'],
+        data() {
+            return {
+                comments: [
+
+                ]
+            }
+        },
+
+        created () {
+            this.printComment()
+            this.$root.$on('comment', message => {
+                this.comments.push(message)
+            })
+        },
+
+        methods: {
+            printComment() {
+                axios
+                .get('/api/posts/' + this.postData.slug)           
+                .then(response => {
+                    this.comments = response.data.comments
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+            },
+
+            prettyDate(date){
+                return momentJs(date).fromNow()
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+    .fade-enter-active, .fade-leave-active {
+        transition: opacity .2s;
+    }
+    .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+        opacity: 0;
+    }
+    [contenteditable='true'] {
+        border: 1px solid var(--color-article);
+        outline: none;
+    }
+</style>
