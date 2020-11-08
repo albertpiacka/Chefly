1bb1d3b1a35af75e43304a054a9895e2c70d7944
albertpiacka <albertpiacka@gmail.com>
2020-11-08 21:25:37 +0100

    Vue admin component for all posts

---
+++ resources/js/admin/views/Posts.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="content-wrapper" data-aos="fade-up">
+        <h1>Posts</h1>
+        <div class="data-table">
+            <table class="table table-striped shadow-lg">
+                <thead class="thead-dark">
+                    <tr>
+                    <th scope="col">#id</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">Text</th>
+                    <th scope="col">Created</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="post in posts" :key="post.id">
+                        <th scope="row">{{post.id}}</th>
+                        <td>{{post.title}}</td>
+                        <td>{{post.text}}</td>
+                        <td>{{prettyDate(post.created_at)}}</td>
+                    </tr>
+                </tbody>    
+            </table>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                posts: [
+
+                ]
+            }
+        },
+
+        created () {
+            axios.get('/api/posts')
+                 .then(response => {
+                     this.posts = response.data
+                 });
+        },
+
+        methods: {
+            prettyDate(date) {
+                return momentJs(date).format("MMM Do YY")
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+    
+</style>
