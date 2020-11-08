f803cf82fbdbc9e47d8b9b16d69f80a77ccd24ec
albertpiacka <albertpiacka@gmail.com>
2020-11-08 21:28:27 +0100

    Admin navbar vue component

---
+++ resources/js/admin/components/TheNavigation.vue
@@ -0,0 +1,33 @@
+<template>
+    <nav class="py-5">
+        <b-card no-body header="Card with flush list group">
+            <b-list-group flush>
+                <b-list-group-item href="#">
+                    <router-link class="text-dark" to="/admin/">Dashboard</router-link>
+                </b-list-group-item>
+
+                <b-list-group-item href="#">
+                    <router-link class="text-dark" to="/admin/posts">Posts</router-link>    
+                </b-list-group-item>
+
+                <b-list-group-item href="#">
+                    <router-link class="text-dark" to="/admin/comments">Comments</router-link>    
+                </b-list-group-item>
+
+                <b-list-group-item href="#">
+                    <router-link class="text-dark" to="/admin/users">Users</router-link>  
+                </b-list-group-item>
+            </b-list-group>
+        </b-card>
+    </nav>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
