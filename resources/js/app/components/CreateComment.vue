2c322bf03d27a1a46fe00a19e2233af0b937815d
albertpiacka <albertpiacka@gmail.com>
2020-11-08 21:22:19 +0100

    Vue component for creating comments (axios etc..)

---
+++ resources/js/app/components/CreateComment.vue
@@ -0,0 +1,44 @@
+
+<script>
+    export default {
+        props: ['post-data'],
+
+        data() {
+            return {
+                text: '',
+                userId: '',
+                postId: '',
+                userName: '',
+                date: ''
+            }
+        },
+
+        mounted () {
+            this.postId = this.$refs.postId.value,
+            this.userId = this.$refs.userId.value,
+            this.userName = this.$refs.userName.value,
+            this.date = momentJs().fromNow()
+        },
+
+        methods: {
+            returnData() {
+                axios.post('/comments', {
+                    post_id: this.postId,
+                    user_id: this.userId,
+                    text: this.text
+                });
+
+                this.$root.$emit('comment', {
+                        'text': this.text,
+                        'id': Math.floor(Math.random() * Math.floor(1000)),
+                        'user': {
+                            'name': this.userName
+                        }
+                });
+
+                this.$refs.textArea.value = ''
+            }
+        },
+    }
+</script>
+
