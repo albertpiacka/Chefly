5333f8f48f31eb066dfaea60276b956176a43845
albertpiacka <albertpiacka@gmail.com>
2020-11-08 21:29:02 +0100

    Search form component

---
+++ resources/js/admin/components/SearchForm.vue
@@ -0,0 +1,20 @@
+<template>
+    <form class="form-inline">
+        <input v-model="message" class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                message: ''
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
