441aff7f36a3a054cfe0002a2b4ca283eef9bbe1
albertpiacka <albertpiacka@gmail.com>
2021-02-03 15:23:46 +0100

    landing page is finished

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,36 +1,22 @@
 @extends('layouts.app', ['class' => 'Chefly'])
 
 @section('content')
-<div class="container py-5">
-    <div class="flex-center position-ref full-height">
-        <div class="content">
-
-            <div class="about-wrapper">
-
-                <blockquote data-aos="zoom-in-up">
-                    <h2 class="headings-primary-dark">What is this about anyway?</h2>
-                    <footer class="text-primary-dark">Chefly is a small project focusing on the world of culinary and gastronomy. Main goal is to gather useful information and share them among people.</footer>
-                 </blockquote>
-
-                 <h2 data-aos="zoom-in-up" class="headings-primary-dark">Chefly lets you..</h3>
-
-                 <blockquote data-aos="zoom-in-up">
-                    <h3 class="headings-primary-dark">Create and customize your articles</h3>
-                    <footer class="text-primary-dark">Chefly has intuitive system of creating articles. It's simple, yet effective!</footer>
-                 </blockquote>
-
-                 <blockquote data-aos="zoom-in-up">
-                    <h3 class="headings-primary-dark">Discuss hot topics with anyone</h3>
-                    <footer class="text-primary-dark">You can see what people have to say about your insight.</footer>
-                 </blockquote>
-
-                 <blockquote data-aos="zoom-in-up">
-                    <h3 class="headings-primary-dark">Share your thoughts through your profile</h3>
-                    <footer class="text-primary-dark">Tell people about yourself!</footer>
-                 </blockquote>
-
-            </div>
-        </div>
+    <div class="wrapper" data-aos="fade-up">
+        <section class="intro">
+            <header data-aos="fade-right">
+                <h1>Publishing platform for food enthusiasts</h1>
+            </header>
+    
+            <main data-aos="fade-right" data-aos-delay="300">
+                <p>
+                    Create beautiful, independent online publication by writing faster and better than you ever have before.
+                </p>
+            </main>
+    
+            <footer data-aos="fade-right" data-aos-delay="500">
+                <b-button pill size="lg" variant="warning" class="text-white" href="{{ route('register') }}">Get started - <small>it's free</small></b-button>
+            </footer>
+        </section>
     </div>
 @endsection
 
