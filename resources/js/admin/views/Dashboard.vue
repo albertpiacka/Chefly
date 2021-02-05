bc3c410a9fd2e99d4e4b7cfbbf23dddbccae3dd6
albertpiacka <albertpiacka@gmail.com>
2021-02-05 09:20:23 +0100

    changed ugly chart to something better.. added most liked post

--- resources/js/admin/views/Dashboard.vue
+++ resources/js/admin/views/Dashboard.vue
@@ -2,18 +2,56 @@
     <div data-aos="fade-up">
         <h1>Dashboard</h1>
 
-        <div class="chart">
-            <canvas id="myChart" width="400" height="400"></canvas>
-        </div>
+        <section class="section-1">
+            <b-card
+                title="Data in pie chart"
+                class="text-dark"
+            >
+                <canvas id="allData"></canvas>
+
+                <router-link to="/admin/users">
+                    <b-button pill variant="primary">Edit users</b-button>
+                </router-link>
+            </b-card>
+
+            <b-card class="text-dark card-2">
+                <header v-if="this.mostLiked">
+                    <h1>
+                        <router-link :to="`/admin/posts/${this.mostLiked[0].slug}`">
+                            <strong>{{this.mostLiked[0].title}}</strong>
+                        </router-link> 
+                        has most likes!
+                    </h1>
+                </header>
+
+                <main v-if="this.mostLiked">
+                    <div
+                        v-html="shorten(this.mostLiked[0].text, 400)"
+                    >
+                    </div>
+
+                    <router-link to="/admin/posts">
+                        <b-button pill variant="primary" class="my-3">View posts</b-button>
+                    </router-link>
+                </main>
+            </b-card>
+        </section>
 
     </div>
 </template>
 
 <script>
+    import tableMixin from '../mixins/tableMixin'
+
     export default {
+        mixins: [tableMixin],
         data() {
             return {
                 posts: [
+
+                ],
+
+                quickposts: [
 
                 ],
 
@@ -23,7 +61,9 @@
 
                 comments: [
 
-                ]
+                ],
+
+                mostLiked: null,
             }
         },
 
@@ -31,61 +71,53 @@
             axios.get('/api/posts')
                  .then(response => {
                      this.posts = response.data
-                     this.addData(myChart, 'posts', response.data.length)
+                     this.allData(allData, 'posts', response.data)
+
+                     this.mostLiked = this.posts.sort((a, b) => {
+                         a = a.likes.length
+                         b = b.likes.length 
+                         var results = a > b ? -1 : a < b ? 1 : 0;
+                         return results
+                     })
+                 })
+
+            axios.get('/api/quickposts')
+                 .then(response => {
+                    this.quickposts = response.data
+                    this.allData(allData, 'quickposts', response.data)
                  })
 
             axios.get('/api/users')
                  .then(response => {
                      this.users = response.data
-                     this.addData(myChart, 'users', response.data.length)
+                     this.allData(allData, 'users', response.data)
                  })
 
             axios.get('/api/comments')
                  .then(response => {
                      this.comments = response.data
-                     this.addData(myChart, 'comments', response.data.length)
+                     this.allData(allData, 'comments', response.data)
                  })
 
-            var ctx = document.getElementById('myChart');
-            var myChart = new Chart(ctx, {
-                type: 'horizontalBar',
+            let ctx1 = document.getElementById('allData');
+
+            let allData = new Chart(ctx1, {
+                type: 'pie',
                 data: {
                     labels: [],
                     datasets: [{
-                        label: '',
-                        data: [],
-                        backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                        ],
-                        borderColor: [
-                            'rgba(255, 99, 132, 1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                        ],
-                        borderWidth: 1
+                        backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9"],
+                        data: []
                     }]
                 },
-                options: {
-                    scales: {
-                        xAxes: [{
-                            ticks: {
-                                beginAtZero: true
-                            }
-                        }]
-                    }
-                }
-            });
+            })
         },
 
         methods: {
-            addData(chart, label, data) {
-                chart.data.labels.push(label);
-                chart.data.datasets.forEach((dataset) => {
-                    dataset.data.push(data);
-                });
-                chart.update();
+            allData(chart, label, data) {
+                chart.data.datasets[0].data.push(data.length)
+                chart.data.labels.push(label)
+                chart.update()
             }
         },
     }
@@ -100,9 +132,38 @@
         }
     }
 
-    .chart {
-        padding: 1em;
+    strong {
+        color: rgba(230, 149, 0, 1);
     }
 
+    .section-1 {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        grid-gap: 1em;
+        padding: 1em;
+
+        @media screen and (max-width: 500px){
+            grid-template-columns: 1fr;
+        }
+
+        .card {
+            border-radius: 2em !important;
+        }
+
+        .card-2 {
+            display: grid;
+            align-content: center;
+
+            h1 {
+                margin: 0;
+            }
+
+            main {
+                margin: 1em 0;
+            }
+        }
+
+
+    }
 
 </style>
