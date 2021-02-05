2d2f1f3620a47d41cc2a787078fd3510f943ecc2
albertpiacka <albertpiacka@gmail.com>
2021-02-05 09:24:01 +0100

    like btn > click instead of dblclick event

--- resources/views/posts/post.blade.php
+++ resources/views/posts/post.blade.php
@@ -38,9 +38,10 @@
         @auth
             <like-post inline-template :post-data="{{$post}}" :user-data="{{Auth::user()}}">
                 <div class="likes-wrapper">
-                    <svg @dblclick="toggleLike" width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-heart-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    <svg @click="toggleLike" width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-heart-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
                     </svg>
+
                     <div class="likes-count">
                         @{{postLikes}}
                     </div>
