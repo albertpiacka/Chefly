399f09b39db6c887298e96cbe5c0b711b1587478
albertpiacka <albertpiacka@gmail.com>
2021-03-08 12:43:25 +0100

    updated migrations

--- database/migrations/2020_11_20_151726_create_likes_table.php
+++ database/migrations/2020_11_20_151726_create_likes_table.php
@@ -15,11 +15,11 @@ class CreateLikesTable extends Migration
     {
         Schema::create('likes', function (Blueprint $table) {
             $table->id();
-            $table->integer('post_id')->unsigned();
-            $table->integer('user_id')->unsigned();
+            $table->unsignedBigInteger('post_id');
+            $table->unsignedBigInteger('user_id');
             $table->timestamps();
-            $table->foreign('post_id')->references('id');
-            $table->foreign('user_id')->references('id');
+            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
