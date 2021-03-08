e75665c821f4b52397aca2cbbbd724d67e0ee10b
albertpiacka <albertpiacka@gmail.com>
2021-03-08 13:18:18 +0100

    updated migrations, user - type integer

--- database/migrations/2019_12_07_152706_create_users_table.php
+++ database/migrations/2019_12_07_152706_create_users_table.php
@@ -21,7 +21,7 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
-            $table->int('type')->default(0);
+            $table->integer('type')->default(0);
         });
     }
 
